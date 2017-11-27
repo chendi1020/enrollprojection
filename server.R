08f8d84c97d18118c160655eb98d8ab788c63f2f
Di Chen <chendi4@opb.msu.edu>
2017-11-27 17:14:59 -0500

    format chart and table

--- server.R
+++ server.R
@@ -84,16 +84,60 @@ shinyServer(function(input, output) {
         
         
         output$trend <- renderHighchart({
-                agg <- datagg()
+                agg <- datagg() %>% mutate(Act= ifelse(Fterm<=1174, headcount, NA),
+                                           project= ifelse(Fterm>1174, headcount, NA))
+                corseq <- c("#7fc97f", "#beaed4", "#fdc086", "#b10026",'#386cb0','#f0027f','#bf5b17','#666666')
+                hc <- highchart() %>%  hc_xAxis(categories = agg$Termcode)
                 if (input$breakdown =='class level'){
-                        hchart(agg, "line", hcaes(x = Termcode, y = headcount, group =classlvl))%>%
-                                hc_yAxis(title=list(text="Headcount"))
+                       # hchart(agg, "line", hcaes(x = Termcode, y = headcount, group =classlvl),
+                        #       color = c("#7fc97f", "#beaed4", "#fdc086", "#b10026",'#386cb0','#f0027f','#bf5b17','#666666'))%>%
+                        #        hc_yAxis(title=list(text="Headcount"))%>% hc_xAxis(title=list(text=""))%>%
+                        #hc_tooltip(table = TRUE, sort = TRUE) %>% hc_add_theme(hc_theme_google())
+                       # hc <- highchart() %>%  hc_xAxis(categories = agg$Termcode)
+                        #corseq <- c("#7fc97f", "#beaed4", "#fdc086", "#b10026",'#386cb0','#f0027f','#bf5b17','#666666')
+                        
+                        
+                        for (i in seq(unique(agg$classlvl))){
+                                hc<- hc%>% hc_add_series( name= paste0('Actual ', unique(agg$classlvl)[i]), type="line",marker =  list(enabled=F),
+                                                          data =  agg[agg$classlvl==unique(agg$classlvl)[i],]$Act, color=corseq[i]) %>%
+                                        
+                                        hc_xAxis(categories =agg[agg$classlvl==unique(agg$classlvl)[i],]$Termcode) %>% 
+                                        hc_yAxis(title=list(text="Headcount"), min=0)%>%
+                                        hc_add_series(name = paste0('Proj ',unique(agg$classlvl)[i]), type='line', dashStyle='longdash',  color=corseq[i],
+                                                      marker =  list(fillColor= '#FFFFFF',
+                                                                     lineWidth= 2,
+                                                                     radius=3,
+                                                                     lineColor= corseq[i],
+                                                                     symbol="circle"),
+                                                      data = agg[agg$classlvl==unique(agg$classlvl)[i],]$project)
+                                
+                        }
+                        
+                        hc
+                        
                 }
                 else {
-                        hchart(agg, "line", hcaes(x =Termcode, y = headcount, group =lvl))%>%
-                                hc_yAxis(title=list(text="Headcount"),
-                                         min=0)
-                }
+                        #hchart(agg, "line", hcaes(x =Termcode, y = headcount, group =lvl))%>%
+                         #       hc_yAxis(title=list(text="Headcount"),
+                          #               min=0)%>% hc_xAxis(title=list(text=""))%>%hc_tooltip(table = TRUE, sort = TRUE) %>% hc_add_theme(hc_theme_google())
+                        for (i in seq(unique(agg$lvl))){
+                                hc<- hc%>% hc_add_series( name= paste0('Actual ', unique(agg$lvl)[i]), type="line",marker =  list(enabled=F),
+                                                          data =  agg[agg$lvl==unique(agg$lvl)[i],]$Act, color=corseq[i]) %>%
+                                        
+                                        hc_xAxis(categories =agg[agg$lvl==unique(agg$lvl)[i],]$Termcode) %>% 
+                                        hc_yAxis(title=list(text="Headcount"), min=0)%>%
+                                        hc_add_series(name = paste0('Proj ',unique(agg$lvl)[i]), type='line', dashStyle='longdash',  color=corseq[i],
+                                                      marker =  list(fillColor= '#FFFFFF',
+                                                                     lineWidth= 2,
+                                                                     radius=3,
+                                                                     lineColor= corseq[i],
+                                                                     symbol="circle"),
+                                                      data = agg[agg$lvl==unique(agg$lvl)[i],]$project)
+                                
+                        }
+                        
+                        hc
+                 }
                 
               
                 
--- ui.R
+++ ui.R
@@ -12,46 +12,49 @@ shinyUI(fluidPage(theme = shinytheme("cerulean"),
                   navbarPage("Enrollment Projection",
                              tabPanel("By Class Level",
                                       fluidRow(
-                                              column(2,
-                                                     br(),
-                                                     #uiOutput('cohort'),
-                                                     numericInput("ftdcu", label = h5("New First Time Undergrad Input"), value = 7890),
-                                                     numericInput("tran", label = h5("New Transfer Undergrad Input"), value = 1550),
-                                                     numericInput("grad", label = h5("New Grad Input"), value = 1750),
-                                                     numericInput("spadd", label = h5("Addtional Spring adds"), value = 0),
-                                                     selectInput("outyr", label = h5("Years to project out"), 
-                                                                 choices = list("3 yrs" = 3, "5 yrs" = 5, "10 yrs" = 10), 
-                                                                 selected = 5),
-                                                     selectInput("breakdown", label = h5("result breakdown by"), 
-                                                                 choices = list("overall", "student level", "class level"), 
-                                                                 selected = "student level"),
-                                                    
-                                                     downloadButton('downloadds', 'Download'),
-                                                     p('updated at 11/22/2017')
-                                                     
-                                                     
-                                                     
-                                              ),
-                                              column(1,
-                                                     br(),
-                                                     
-                                                     sliderInput("wsp1", label = h6("Fall to Spring Weight 1"), min = 0, 
-                                                                 max = 1, step = 0.25, value = 1),
-                                                     
-                                                     sliderInput("wsp2", label = h6("Fall to Spring Weight 2"), min = 0, 
-                                                                 max = 1, step = 0.25,value = 0),
-                                                     sliderInput("wsp3", label = h6("Fall to Spring Weight 3"), min = 0, 
-                                                                 max = 1,step = 0.25, value = 0),
-                                                     sliderInput("wfa1", step = 0.25,label = h6("Spring to Fall Weight 1"), min = 0, 
-                                                                 max = 1, value = 0.75),
-                                                     sliderInput("wfa2", step = 0.25,label = h6("Spring to Fall Weight 2"), min = 0, 
-                                                                 max = 1, value = 0.25),
-                                                     sliderInput("wfa3", label = h6("Spring to Fall Weight 3"), min = 0, 
-                                                                 max = 1,step = 0.25, value = 0)
-                                                     
-                                                     
-                                                     
-                                              ),
+                                              column(3,
+                                                     column(6,
+                                                            br(),
+                                                            #uiOutput('cohort'),
+                                                            numericInput("ftdcu", label = h5("New First Time Undergrad Input"), value = 7890),
+                                                            numericInput("tran", label = h5("New Transfer Undergrad Input"), value = 1550),
+                                                            numericInput("grad", label = h5("New Grad Input"), value = 1750),
+                                                            numericInput("spadd", label = h5("Addtional Spring adds"), value = 0),
+                                                            selectInput("outyr", label = h5("Years to project out"), 
+                                                                        choices = list("3 yrs" = 3, "5 yrs" = 5, "10 yrs" = 10), 
+                                                                        selected = 5),
+                                                            selectInput("breakdown", label = h5("result breakdown by"), 
+                                                                        choices = list("overall", "student level", "class level"), 
+                                                                        selected = "student level"),
+                                                            
+                                                            downloadButton('downloadds', 'Download'),
+                                                            
+                                                            p(paste('updated at ', Sys.Date()))
+                                                            
+                                                            
+                                                            
+                                                     ),
+                                                     column(6,
+                                                            br(),
+                                                            
+                                                            sliderInput("wsp1", label = h6("Fall to Spring Weight 1"), min = 0, 
+                                                                        max = 1, step = 0.25, value = 1),
+                                                            
+                                                            sliderInput("wsp2", label = h6("Fall to Spring Weight 2"), min = 0, 
+                                                                        max = 1, step = 0.25,value = 0),
+                                                            sliderInput("wsp3", label = h6("Fall to Spring Weight 3"), min = 0, 
+                                                                        max = 1,step = 0.25, value = 0),
+                                                            sliderInput("wfa1", step = 0.25,label = h6("Spring to Fall Weight 1"), min = 0, 
+                                                                        max = 1, value = 0.75),
+                                                            sliderInput("wfa2", step = 0.25,label = h6("Spring to Fall Weight 2"), min = 0, 
+                                                                        max = 1, value = 0.25),
+                                                            sliderInput("wfa3", label = h6("Spring to Fall Weight 3"), min = 0, 
+                                                                        max = 1,step = 0.25, value = 0)
+                                                            
+                                                            
+                                                            
+                                                     )    )
+                                              ,
                                               column(7,
                                                      tabsetPanel(
                                                              tabPanel('Chart',
